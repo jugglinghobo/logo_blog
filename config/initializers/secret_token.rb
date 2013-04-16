6f46609055ab5346594f31a7da2e0c3243d4b0a3
Simon Kiener <jugglinghobo@gmail.com>
2013-04-16 17:30:33 +0200

    initial commit, added heroku database, removed sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LogoBlog::Application.config.secret_token = '32234b931df69ddb6afe1aa9c5e92d835c7326f213b7027f285518f922c3e68c3386dbe5fcde6260ccc6c42360b66dce98739b46e547488ed3e82622e0e37466'
