d1c257f1cb7201018859486a2a65a49198ec9025
Simon Kiener <jugglinghobo@gmail.com>
2013-04-20 20:55:12 +0200

    added relation posts/user, migrated, moved some stuff to haml

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content
+  
+  belongs_to :user
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
+  has_many :posts
+
   validates_presence_of :email, :name
 end
